e2dd8d836c20ceff4cdeb24f01097d21cb132ad8
Daniel Lim <hbt@markhamboards.com>
2016-04-24 16:05:57 -0400

    update models

--- scripts/scripts.689bbc0a.js
+++ scripts/scripts.689bbc0a.js
@@ -1,5 +1,5 @@
 "use strict";
-angular.module("mytodoApp", ["ngAnimate", "ngAria", "ngCookies", "ngMessages", "ngResource", "ngRoute", "ngSanitize", "ngTouch", "ui.sortable", "LocalStorageModule"]).config(["localStorageServiceProvider", function(a) {
+angular.module("portfolioApp", ["ngAnimate", "ngAria", "ngCookies", "ngMessages", "ngResource", "ngRoute", "ngSanitize", "ngTouch", "ui.sortable", "LocalStorageModule"]).config(["localStorageServiceProvider", function(a) {
   a.setPrefix("ls")
 }]).config(["$routeProvider", function(a) {
   a.when("/", {
@@ -11,15 +11,15 @@ angular.module("mytodoApp", ["ngAnimate", "ngAria", "ngCookies", "ngMessages", "
   }).otherwise({
     redirectTo: "/"
   })
-}]), angular.module("mytodoApp").controller("MainCtrl", ["$scope", "localStorageService", function(a, b) {
-  var c = b.get("todos");
-  a.todos = c || [], a.$watch("todos", function() {
-    b.set("todos", a.todos)
-  }, !0), a.addTodo = function() {
-    a.todos.push(a.todo), a.todo = ""
-  }, a.removeTodo = function(b) {
-    a.todos.splice(b, 1)
+}]), angular.module("portfolioApp").controller("MainCtrl", ["$scope", "localStorageService", function(a, b) {
+  var c = b.get("portfolios");
+  a.portfolios = c || [], a.$watch("portfolios", function() {
+    b.set("portfolios", a.portfolios)
+  }, !0), a.addPortfolios = function() {
+    a.portfolios.push(a.portfolio), a.portfolio = ""
+  }, a.removePortfolios = function(b) {
+    a.portfolios.splice(b, 1)
   }
-}]), angular.module("mytodoApp").controller("AboutCtrl", ["$scope", function(a) {
+}]), angular.module("portfolioApp").controller("AboutCtrl", ["$scope", function(a) {
   a.awesomeThings = ["HTML5 Boilerplate", "AngularJS", "Karma"]
 }]);
